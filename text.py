7692775b157f6aec9397c54ff0c24ded0178747b
Rick Lively <rslively2003@yahoo.com>
2017-01-30 20:46:12 -0700

    Added comment

--- text.py
+++ text.py
@@ -14,6 +14,7 @@ n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
 plt.xlabel('Smarts')
 plt.ylabel('Probability')
 plt.title('Histogram of IQ')
+# Add text at a specific position
 plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([40, 160, 0, 0.03])
 plt.grid(True)
